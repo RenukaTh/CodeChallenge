0e457443ab41dcc95529cb41e58d3e9347fd01e6
RenukaTh <53560354+RenukaTh@users.noreply.github.com>
2019-08-01 14:50:39 +0530

    Add files via upload
    Submitting the file

---
+++ IsATraingle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge
+{
+    class IsATraingle
+    {
+
+        //int a, b, c;
+        public static void Main(String[] args)
+        {
+            int a = 7, b = 10, c = 5;
+
+            if (checkValidity(a, b, c))
+            {
+                Console.WriteLine("Valid traingle");
+            }
+            else
+                Console.WriteLine("Invalid triangle");
+
+        }
+
+
+        static Boolean checkValidity(int a, int b, int c)
+        {
+            // check condition 
+            if (a + b <= c || a + c <= b || b + c <= a)
+                return false;
+            else
+                return true;
+        }
+    }
+}
